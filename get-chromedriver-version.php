cff385e8c6c99b0d4893dfe97f24ab08c8229f76
iconicinfotech <105965991+iconicinfotech@users.noreply.github.com>
2023-07-21 20:01:50 +0530

    Update get-chromedriver-version.php

--- get-chromedriver-version.php
+++ get-chromedriver-version.php
@@ -5,19 +5,44 @@ function myfilter($value){
 					$newphrase = htmlspecialchars($newphrase);
 					return $newphrase;
 				   }
-$verArray=array("115"=>"115.0.5790.98");
-
+				   
 if(isset($_GET["ver"])){
      $ver=myfilter($_GET["ver"]);
     if($ver>=115 && strlen($ver)==3){
-        echo $verArray[$ver];
+        $isExist=false;
+        $fname="versions.txt";
+        $file=fopen($fname,"a+") or die("Unable to open file!");
+        while (($line = fgets($file)) !== false) {
+            if(strpos($line, $ver)!== false){
+                $isExist=true;
+                echo $line;
+                break;
+            }
+        }
+        if(!$isExist){ echo "Not Found"; }
+        fclose($file);
+        
+        
     }else{ echo "ver not valid"; }
 }else{ 
     
  $siteData=file_get_contents("https://googlechromelabs.github.io/chrome-for-testing/");
 $siteData=explode("^",str_replace("<code>","^",$siteData))[1];   
     if(count(explode(".",$siteData))>1){
-        echo str_replace("</code><td>","",$siteData);
+        $isExist=false;
+        $fname="versions.txt";
+        $release= str_replace("</code><td>","",$siteData);
+        $file=fopen($fname,"a+") or die("Unable to open file!");
+        while (($line = fgets($file)) !== false) {
+            if($release==trim($line)){
+                $isExist=true;
+                break;
+            }
+        }
+        if(!$isExist){
+            fwrite($file, $release.PHP_EOL);
+        }else{echo "Already Exist"; }
+        fclose($file);
     }
     
 }
